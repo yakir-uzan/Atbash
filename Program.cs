2ac7cde15eb2709ce67115ad25555cc20251a97d
David Tiechman <d0534110171@gmail.com>
2025-05-14 15:50:56 +0300

    function signature

--- Program.cs
+++ Program.cs
@@ -8,13 +8,36 @@ namespace Atbash
 {
     internal class Program
     {
-        static string newstring(string s)
+        static Dictionary<string,int> CounterString(string []s,string [] wordlist)
         {
-            s = s.Trim();
-            return s;
+            Dictionary <string,int> problematicwords = new Dictionary<string,int>();
+            foreach (string word in s) {
+                if (wordlist.Contains(word))
+                    if (problematicwords.ContainsKey(word))
+                    {
+                        problematicwords[word] = 1;
+                    }
+                else problematicwords[word]++;
+                
+                
+            }
+
+            return problematicwords;
+        }
+        static void print(Dictionary<string,int> d)
+        {
+               foreach (var word in d)
+            {
+                Console.WriteLine(word);
+            }
         }
         static void Main(string[] args)
         {
+            Dictionary<string,int>dd = new Dictionary<string,int>();
+            dd["chamas"] = 1;
+            dd["pzzatzza"] = 5;
+
+            print(dd);
         }
     }
 }
