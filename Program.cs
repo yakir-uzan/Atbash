ff78c1109c0f072d16b4fb60d08c53eaa2b6b9ca
David Tiechman <d0534110171@gmail.com>
2025-05-14 15:56:19 +0300

    text

--- Program.cs
+++ Program.cs
@@ -8,36 +8,35 @@ namespace Atbash
 {
     internal class Program
     {
-        static Dictionary<string,int> CounterString(string []s,string [] wordlist)
+        static Dictionary<string, int> CounterString(string[] s, string[] wordlist)
         {
-            Dictionary <string,int> problematicwords = new Dictionary<string,int>();
-            foreach (string word in s) {
+            Dictionary<string, int> problematicwords = new Dictionary<string, int>();
+            foreach (string word in s)
+            {
                 if (wordlist.Contains(word))
                     if (problematicwords.ContainsKey(word))
                     {
                         problematicwords[word] = 1;
                     }
-                else problematicwords[word]++;
-                
-                
+                    else problematicwords[word]++;
             }
 
             return problematicwords;
         }
-        static void print(Dictionary<string,int> d)
+        static void print(Dictionary<string, int> d)
         {
-               foreach (var word in d)
+            foreach (var word in d)
             {
                 Console.WriteLine(word);
             }
         }
         static void Main(string[] args)
         {
-            Dictionary<string,int>dd = new Dictionary<string,int>();
-            dd["chamas"] = 1;
-            dd["pzzatzza"] = 5;
+            //Dictionary<string,int>dd = new Dictionary<string,int>();
+            //dd["chamas"] = 1;
+            //dd["pzzatzza"] = 5;
 
-            print(dd);
+            //print();        }
         }
     }
 }
