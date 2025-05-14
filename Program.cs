0200886559f7c352748283eac38ab70693de5539
David Tiechman <d0534110171@gmail.com>
2025-05-14 17:51:10 +0300

    finech functsion

--- Program.cs
+++ Program.cs
@@ -8,41 +8,27 @@ namespace Atbash
 {
     internal class Program
     {
-        static Dictionary<string, int> CounterString(string[] s, string[] wordlist)
+        static (string[],int) CounterString(string[] s, string[] wordlist)
         {
-            Dictionary<string, int> problematicwords = new Dictionary<string, int>();
+            int counter = 0;
             foreach (string word in s)
             {
-                if (wordlist.Contains(word))
-                {
-                    if (problematicwords.ContainsKey(word))
-                    {
-                        problematicwords[word] ++;
-                    }
-                    else problematicwords[word] = 1;
-                }
-                else problematicwords[word] = 0;
+                if (wordlist.Contains(word)) 
+                    counter++;
             }
-                print(problematicwords);
-                        return problematicwords;
+                                   return (s,counter); 
         }
-        static void print(Dictionary<string, int> d)
-        {
-            foreach (var word in d)
-            {
-                Console.WriteLine(word);
-            }
+              static void Finitefunctsoin(string[] arrya,int num)
+                    {
+                Console.WriteLine($"{arrya} {num}");
+            
         }
         static void Main(string[] args)
         {
             string[] words = { "chamas", "til", "ptzatzza", "muchamd", "chamas", "til", "chamas", "ptzatzza", "chamas" };
          string[] problems = { "chamas", "ptzatzza" };
-            CounterString(words, problems);
-            //Dictionary<string,int>dd = new Dictionary<string,int>();
-            //dd["chamas"] = 1;
-            //dd["pzzatzza"] = 5;
-
-            //print();        }
+            var (arr,num) = CounterString(words, problems);
+            Finitefunctsoin(arr, num);
         }
     }
 }
