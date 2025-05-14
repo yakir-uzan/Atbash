9a5e335da19a945d74328c8fdf1778c1e9f413af
Yakir Uzan <yakiruzangreen@gmail.com>
2025-05-14 17:28:50 +0300

    changed the return of the function to return an array and not a string

--- Program.cs
+++ Program.cs
@@ -9,20 +9,27 @@ namespace Atbash
 {
     internal class Program
     {
-        static string DecryptedText(string txt)
+        static string[] convertText(string txt)
         {
             string txtLower = txt.ToLower();
-            string result = " ";
+            string result = "";
 
             foreach (char c in txtLower)
-                result += (char)('z' - (c - 'a'));
-            return result;
+            {
+                if (c >= 'a' && c <= 'z')
+                    result += (char)('z' - (c - 'a'));
+
+                else
+                    result += c;
+            }
+            string[] convTxt = result.Split(' ');
+            return convTxt;
         }
     
 
         static void Main(string[] args)
         {
-            Console.WriteLine(DecryptedText("abcd bvcx"));
+            Console.WriteLine(convertText("\r\nLfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb. \r\nGsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo. \r\nYlnyh szev yvvm kozxvw mvzi pvb olxzgrlmh. \r\nMfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm. \r\nGsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt.\r\nDv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg.\r\nErxglib rh mvzi. Hgzb ivzwb.\r\n"));
         }
     }
 }
